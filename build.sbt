671ad2c22382e148642fc1dab4424b278eb26bce
Федин Максим Владимирович <Fedin.M.V@sberbank.ru>
2020-12-21 13:15:24 +0300

    Upickle example has been added.

--- build.sbt
+++ build.sbt
@@ -4,10 +4,18 @@ version := "0.1"
 
 scalaVersion := "2.13.3"
 
-val autowireVersion = "0.3.0"
+val circeVersion = "0.12.3"
+val autowireVersion = "0.3.2"
+val upickleVersion = "0.9.5"
+
 
 libraryDependencies += "com.lihaoyi" %% "autowire" % autowireVersion
-libraryDependencies += "com.lihaoyi" %% "upickle" % "0.9.5"
+libraryDependencies += "com.lihaoyi" %% "upickle" % upickleVersion
+libraryDependencies ++= Seq(
+    "io.circe" %% "circe-core",
+    "io.circe" %% "circe-generic",
+    "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
 
 
 
---
+++ src/main/scala/ru.maksimfedin.autowireExperiments/Upickle.scala
@@ -0,0 +1,42 @@
+package ru.maksimfedin.autowireExperiments
+
+
+import autowire._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+// shared API interface
+trait MyApi {
+    def doThing(i: Int, s: String): Seq[String]
+}
+
+object MyApiImpl extends MyApi {
+    def doThing(i: Int, s: String) = Seq.fill(i)(s)
+}
+
+object MyServer extends autowire.Server[String, upickle.default.Reader, upickle.default.Writer] {
+    def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+    def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
+
+    val routes = MyServer.route[MyApi](MyApiImpl)
+}
+
+// client-side implementation, and call-site
+object MyClient extends autowire.Client[String, upickle.default.Reader, upickle.default.Writer] {
+    def write[Result: upickle.default.Writer](r: Result) = upickle.default.write(r)
+
+    def read[Result: upickle.default.Reader](p: String) = upickle.default.read[Result](p)
+
+    override def doCall(req: Request): Future[String] = {
+        println(req)
+        MyServer.routes.apply(req)
+    }
+}
+
+object Main extends App {
+    MyClient[MyApi].doThing(3, "lol").call().foreach(println)
+}
+
+// List(lol, lol, lol)
